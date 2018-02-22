59219dee84275ada6852384202e028d45da6dc6f
Anton Shumsky <anton.shumsky@gmail.com>
2018-02-22 18:26:22 +0200

    1.16: use successfive squaring

--- 1_16_fast_expt.rkt
+++ 1_16_fast_expt.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
 (define (fast-expt b n)
-  (if (= n 0) 1 (* b (fast-expt b (- n 1)))))
+  (if (even n) (square (fast-expt b (/ n 2))) 
+      (if (= n 1) b (* b (fast-expt b (- n 1))))))
+
+(define (square n) (* n n))
+
+(define (even n)
+  (= (remainder n 2) 0))
 
 (provide fast-expt)
