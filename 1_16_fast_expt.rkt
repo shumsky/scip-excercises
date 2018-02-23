bb767af1192928dd226c92b515472d68a9df8806
Anton Shumsky <anton.shumsky@gmail.com>
2018-02-23 21:15:42 +0200

    use iterative algorithm

--- 1_16_fast_expt.rkt
+++ 1_16_fast_expt.rkt
@@ -1,12 +1,17 @@
 #lang racket
 
 (define (fast-expt b n)
-  (if (even n) (square (fast-expt b (/ n 2))) 
-      (if (= n 1) b (* b (fast-expt b (- n 1))))))
+  (fast-expt-iter b n 1))
+
+(define (fast-expt-iter b n a)
+  (cond
+    [(= n 0) a]
+    [(even? n) (fast-expt-iter (square b) (/ n 2) a)]
+    [else (fast-expt-iter b (- n 1) (* a b))]))
 
 (define (square n) (* n n))
 
-(define (even n)
+(define (even? n)
   (= (remainder n 2) 0))
 
 (provide fast-expt)
